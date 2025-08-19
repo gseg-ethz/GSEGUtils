014c8592456c5bf775153d89f2c5b84b8d3ba5b2
Nicholas Meyer <nixton.meyer@gmail.com>
2025-08-19 10:47:28 +0200

    fix: include all submodules in `__init__`

--- src/GSEGUtils/__init__.py
+++ src/GSEGUtils/__init__.py
@@ -1,10 +1,17 @@
 __all__ = [
-    "__version__",
-    "base_arrays", "base_types", "constants", "validators"
+    "__author__", "__email__",
+    "__version__", "version", "__version_tuple__", "version_tuple",
+    "base_arrays", "base_types", "config", "constants",
+    "generate_init_stubs", "logging_setup", "singleton",
+    "util", "validators"
 ]
 
 __author__ = "Nicholas Meyer"
 __email__ = "meyernic@ethz.ch"
 
-from ._version import __version__
-from . import base_arrays, base_types, constants, validators
+from ._version import __version__, version, __version_tuple__, version_tuple
+from . import (
+    base_arrays, base_types, config, constants,
+    generate_init_stubs, logging_setup, singleton,
+    util, validators
+)
